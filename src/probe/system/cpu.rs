be3bf0d908905f5b08e3aa8c4585ca5c5e570bdc
Alexander Thaller <alexander.thaller@trivago.com>
2022-11-16 14:02:07 +0100

    Use counter for cpu metrics

--- src/probe/system/cpu.rs
+++ src/probe/system/cpu.rs
@@ -1,20 +1,12 @@
-use std::time::Duration;
-
 use anyhow::Error;
 use axum::extract::Query;
 use prometheus::{
-    register_gauge_with_registry,
     register_int_gauge_with_registry,
     Encoder,
     Registry,
     TextEncoder,
 };
 use serde::Deserialize;
-use systemstat::{
-    Platform,
-    System,
-};
-use tokio::time::sleep;
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct Params {}
@@ -37,41 +29,66 @@ pub(crate) async fn handler(Query(_params): Query<Params>) -> Vec<u8> {
 
 impl Cpu {
     pub(super) async fn run(registry: &Registry) -> Result<(), Error> {
-        let sys = System::new();
-
-        let cpu = sys.cpu_load_aggregate()?;
-        sleep(Duration::from_secs(1)).await;
-        let cpu = cpu.done()?;
-
-        register_gauge_with_registry!("system_cpu_user", "system cpu user usage", registry)?
-            .set(cpu.user.into());
-
-        register_gauge_with_registry!("nice_cpu_nice", "nice cpu nice usage", registry)?
-            .set(cpu.nice.into());
-
-        register_gauge_with_registry!("system_cpu_system", "system cpu system usage", registry)?
-            .set(cpu.system.into());
-
-        register_gauge_with_registry!(
-            "system_cpu_interrupt",
-            "system cpu interrupt usage",
-            registry
-        )?
-        .set(cpu.interrupt.into());
-
-        register_gauge_with_registry!("idle_cpu_interrupt", "idle cpu interrupt usage", registry)?
-            .set(cpu.idle.into());
-
-        #[cfg(target_os = "linux")]
-        register_gauge_with_registry!("system_cpu_iowait", "system cpu iowait usage", registry)?
-            .set(cpu.platform.iowait.into());
-
         register_int_gauge_with_registry!(
             "system_cpu_core_count",
             "how many cpus are available",
             registry
         )?
         .set(num_cpus::get() as i64);
+
+        Cpu::set_metrics(registry).await
+    }
+
+    #[cfg(target_os = "linux")]
+    async fn set_metrics(registry: &Registry) -> Result<(), Error> {
+        use prometheus::register_int_counter_with_registry;
+        use tokio::{
+            fs::File,
+            io::{
+                AsyncBufReadExt,
+                BufReader,
+            },
+        };
+
+        let file = File::open("/proc/stat").await?;
+        let mut reader = BufReader::new(file);
+        let mut cpu_line = String::new();
+        reader.read_line(&mut cpu_line).await?;
+
+        let mut split = cpu_line.split_ascii_whitespace();
+        split.next();
+
+        let user = split.next().unwrap().parse()?;
+        register_int_counter_with_registry!("system_cpu_user", "system cpu user usage", registry)?
+            .inc_by(user);
+
+        let nice = split.next().unwrap().parse()?;
+        register_int_counter_with_registry!("system_cpu_nice", "system cpu nice usage", registry)?
+            .inc_by(nice);
+
+        let system = split.next().unwrap().parse()?;
+        register_int_counter_with_registry!(
+            "system_cpu_system",
+            "system cpu system usage",
+            registry
+        )?
+        .inc_by(system);
+
+        let idle = split.next().unwrap().parse()?;
+        register_int_counter_with_registry!("system_cpu_idle", "system cpu idle usage", registry)?
+            .inc_by(idle);
+
+        let iowait = split.next().unwrap().parse()?;
+        register_int_counter_with_registry!(
+            "system_cpu_iowait",
+            "system cpu iowait usage",
+            registry
+        )?
+        .inc_by(iowait);
+
+        let irq = split.next().unwrap().parse()?;
+        register_int_counter_with_registry!("system_cpu_irq", "system cpu irq usage", registry)?
+            .inc_by(irq);
 
         Ok(())
     }
