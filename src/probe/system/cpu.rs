bd262343850cf6449d3945a10eaeb201810ab57b
Alexander Thaller <alexander.thaller@trivago.com>
2022-11-17 10:13:59 +0100

    Add system_cpu_other

--- src/probe/system/cpu.rs
+++ src/probe/system/cpu.rs
@@ -9,7 +9,6 @@ use prometheus::{
 };
 use serde::Deserialize;
 use systemstat::{
-    CpuTime,
     Platform,
     System,
 };
@@ -36,36 +35,15 @@ pub(crate) async fn handler(Query(_params): Query<Params>) -> Vec<u8> {
 impl Cpu {
     pub(super) fn run(registry: &Registry) -> Result<(), Error> {
         let sys = System::new();
-        let cpu = sys.cpu_time()?;
-        let cpu = cpu.into_iter().fold(
-            CpuTime {
-                user: 0,
-                nice: 0,
-                system: 0,
-                interrupt: 0,
-                idle: 0,
-                other: 0,
-            },
-            |acc, x| CpuTime {
-                user: acc.user + x.user,
-                nice: acc.nice + x.nice,
-                system: acc.system + x.system,
-                interrupt: acc.interrupt + x.interrupt,
-                idle: acc.idle + x.idle,
-                other: acc.other + x.other,
-            },
-        );
+        let cpu = sys.cpu_time_aggregate()?;
+
+        dbg!(&cpu);
 
         register_int_counter_with_registry!("system_cpu_user", "system cpu user usage", registry)?
             .inc_by(cpu.user.try_into().unwrap());
 
-        register_int_counter_with_registry!(
-            "system_cpu_nice",
-            "system cpu
-        nice usage",
-            registry
-        )?
-        .inc_by(cpu.nice.try_into().unwrap());
+        register_int_counter_with_registry!("system_cpu_nice", "system cpu nice usage", registry)?
+            .inc_by(cpu.nice.try_into().unwrap());
 
         register_int_counter_with_registry!(
             "system_cpu_system",
@@ -74,21 +52,18 @@ impl Cpu {
         )?
         .inc_by(cpu.system.try_into().unwrap());
 
-        register_int_counter_with_registry!(
-            "system_cpu_irq",
-            "system cpu irq
-        usage",
-            registry
-        )?
-        .inc_by(cpu.interrupt.try_into().unwrap());
+        register_int_counter_with_registry!("system_cpu_irq", "system cpu irq usage", registry)?
+            .inc_by(cpu.interrupt.try_into().unwrap());
+
+        register_int_counter_with_registry!("system_cpu_idle", "system cpu idle usage", registry)?
+            .inc_by(cpu.idle.try_into().unwrap());
 
         register_int_counter_with_registry!(
-            "system_cpu_idle",
-            "system cpu
-        idle usage",
+            "system_cpu_other",
+            "system cpu other usage",
             registry
         )?
-        .inc_by(cpu.idle.try_into().unwrap());
+        .inc_by(cpu.other.try_into().unwrap());
 
         register_int_gauge_with_registry!(
             "system_cpu_core_count",
